761b3f5106a3c7b6a801da2c10e18a3632fadf97
GP <gp2856@users.no-reply.github.com>
2017-11-30 19:46:35 -0600

    implemented Vec2 class with operator definitions for addition, subtraction, equality, and scaling

---
+++ Engine/Vec2.h
@@ -0,0 +1,21 @@
+#pragma once
+
+class Vec2
+{
+public:
+	Vec2() = default;
+	Vec2(float x_in, float y_in);
+	Vec2 operator+(const Vec2& rhs) const;
+	Vec2 operator-(const Vec2& rhs) const;
+	Vec2 operator*(const float rhs) const;
+	bool operator==(const Vec2& rhs) const;
+
+	Vec2& operator+=(const Vec2& rhs);
+	Vec2& operator-=(const Vec2& rhs);
+	Vec2& operator*=(const float rhs);
+	
+	
+public:
+	float x;
+	float y;
+};
